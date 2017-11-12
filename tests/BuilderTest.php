f096d98685018fe2954664d1cf13b2ad3ccc8621
Michael Cordingley <Michael.Cordingley@gmail.com>
2017-11-12 12:17:34 -0500

    Whitespace cleanup.

--- tests/BuilderTest.php
+++ tests/BuilderTest.php
@@ -13,18 +13,18 @@ final class BuilderTest extends TestCase
     public function testSimpleTree()
     {
         $values = [
-            [true, false, false, true, 'Some', '$$$', false, true, 'French', '0-10', true],
-            [true, false, false, true, 'Full', '$', false, false, 'Thai', '30-60', false],
-            [false, true, false, false, 'Some', '$', false, false, 'Burger', '0-10', true],
-            [true, false, true, true, 'Full', '$', true, false, 'Thai', '10-30', true],
-            [true, false, true, false, 'Full', '$$$', false, true, 'French', '>60', false],
-            [false, true, false, true, 'Some', '$$', true, true, 'Italian', '0-10', true],
-            [false, true, false, false, 'None', '$', true, false, 'Burger', '0-10', false],
-            [false, false, false, true, 'Some', '$$', true, true, 'Thai', '0-10', true],
-            [false, true, true, false, 'Full', '$', true, false, 'Burger', '>60', false],
-            [true, true, true, true, 'Full', '$$$', false, true, 'Italian', '10-30', false],
-            [false, false, false, false, 'None', '$', false, false, 'Thai', '0-10', false],
-            [true, true, true, true, 'Full', '$', false, false, 'Burger', '30-60', true],
+            [true,  false, false, true,  'Some', '$$$', false, true,  'French',  '0-10',  true],
+            [true,  false, false, true,  'Full', '$',   false, false, 'Thai',    '30-60', false],
+            [false, true,  false, false, 'Some', '$',   false, false, 'Burger',  '0-10',  true],
+            [true,  false, true,  true,  'Full', '$',   true,  false, 'Thai',    '10-30', true],
+            [true,  false, true,  false, 'Full', '$$$', false, true,  'French',  '>60',   false],
+            [false, true,  false, true,  'Some', '$$',  true,  true,  'Italian', '0-10',  true],
+            [false, true,  false, false, 'None', '$',   true,  false, 'Burger',  '0-10',  false],
+            [false, false, false, true,  'Some', '$$',  true,  true,  'Thai',    '0-10',  true],
+            [false, true,  true,  false, 'Full', '$',   true,  false, 'Burger',  '>60',   false],
+            [true,  true,  true,  true,  'Full', '$$$', false, true,  'Italian', '10-30', false],
+            [false, false, false, false, 'None', '$',   false, false, 'Thai',    '0-10',  false],
+            [true,  true,  true,  true,  'Full', '$',   false, false, 'Burger',  '30-60', true],
         ];
 
         $keyExample = function(array $example): array {
