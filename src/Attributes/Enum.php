b734d60b5d3766ebfa22783952c623f9bb3d46b6
Michael Cordingley <Michael.Cordingley@gmail.com>
2017-11-12 11:41:16 -0500

    Inline method.

--- src/Attributes/Enum.php
+++ src/Attributes/Enum.php
@@ -21,20 +21,10 @@ final class Enum extends Base
      */
     public function partition(array $examples): array
     {
-        return $this->groupBy($examples, $this->key);
-    }
-
-    /**
-     * @param array[] $list
-     * @param string $key
-     * @return array[]
-     */
-    final protected function groupBy(array $list, string $key): array
-    {
         $groups = [];
 
-        foreach ($list as $item) {
-            $keyValue = $item[$key];
+        foreach ($examples as $item) {
+            $keyValue = $item[$this->key];
 
             if (!isset($groups[$keyValue])) {
                 $groups[$keyValue] = [];
