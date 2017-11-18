30287cc2c9e8a84d51f076b9696dd39d3859ff18
Michael Cordingley <Michael.Cordingley@gmail.com>
2017-11-18 15:04:58 -0500

    Reorder functions.

--- src/ImportanceMeasures/Base.php
+++ src/ImportanceMeasures/Base.php
@@ -5,17 +5,6 @@ namespace MCordingley\DecisionTree\ImportanceMeasures;
 abstract class Base implements ImportanceMeasure
 {
     /**
-     * @param float[] $proportions e.g. [0.8, 0.1, 0.05, 0.05]
-     * @return float
-     */
-    final protected function entropy(array $proportions): float
-    {
-        return array_reduce($proportions, function ($carry, $proportion) {
-            return $carry - ($proportion > 0 ? $proportion * log($proportion, 2) : 0);
-        }, 0.0);
-    }
-
-    /**
      * @param array $examples
      * @param string $outcomeAttribute
      * @return array e.g. [0.8, 0.1, 0.05, 0.05]
@@ -32,5 +21,16 @@ abstract class Base implements ImportanceMeasure
         return array_map(function (int $count) use ($total): float {
             return $count / $total;
         }, $counts);
+    }
+
+    /**
+     * @param float[] $proportions e.g. [0.8, 0.1, 0.05, 0.05]
+     * @return float
+     */
+    final protected function entropy(array $proportions): float
+    {
+        return array_reduce($proportions, function ($carry, $proportion) {
+            return $carry - ($proportion > 0 ? $proportion * log($proportion, 2) : 0);
+        }, 0.0);
     }
 }
